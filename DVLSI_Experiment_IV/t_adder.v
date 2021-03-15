0dd655cc9add9c1317f8bfacdd8621549bcf2eb4
jaybee-117 <unofficialme117@gmail.com>
2021-03-15 13:38:01 +0530

    fixed timing issue

--- DVLSI_Experiment_IV/t_adder.v
+++ DVLSI_Experiment_IV/t_adder.v
@@ -9,7 +9,7 @@ module t_adder (
     wire cout_cla,cout_rc;
 
     cla_adder cla_1 (.in1(a),.in2(b),.cin(cin),.out(s_cla),.cout(cout_cla));
-    //rc_adder rc_1(a,b,cin,s_rc,cout_rc);
+    rc_adder rc_1(.in1(a),.in2(b),.cin(cin),.out(s_rc),.cout(cout_rc));
 
     initial begin
         $dumpfile("adder_cla_rc.vcd");
@@ -17,17 +17,17 @@ module t_adder (
         cin <=0;
         a <= 4'b0010;
         b <= 4'b1110;
-        #15;
+        #25;
         a <= 4'b0010;
         b <= 4'b1010;
-        #15;
+        #25;
         a <= 4'b0010;
         b <= 4'b0111;
-        #15;
+        #25;
         a <= 4'b0110;
         b <= 4'b0110;
         //$monitor(" a = %b \n b = %b\n sum_cla = %b\n sum_rc = %b\n",a,b,s_cla,s_rc);
-        #15;
+        #25;
         $finish;
 
     end
